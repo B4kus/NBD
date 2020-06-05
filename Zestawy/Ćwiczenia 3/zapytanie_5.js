a284325cdda53d8dba5b77ab11c67bb3325c4355
Szymon Szysz <szysz.szymon@gmail.com>
2020-06-05 19:17:48 +0200

    Update js files

--- Zestawy/Ćwiczenia 3/zapytanie_5.js
+++ Zestawy/Ćwiczenia 3/zapytanie_5.js
@@ -1,17 +1,4 @@
-{\rtf1\ansi\ansicpg1250\cocoartf2512
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red237\green236\blue236;\red52\green56\blue63;
-\red0\green0\blue0;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;\cssrgb\c94510\c94118\c94118;\cssrgb\c26667\c28627\c31373;
-\csgray\c0;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww26280\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+printjson​(​db​.​people​.​find​({ birth_date​:​{$gte​:​"​2000-01-01T00:00:00Z​"​}},{first_name​:​1​, last_name​:​1​,​"​location.city​"​:​1​, _id​:​0​ }).​toArray​())
 
-\f0\fs30 \cf2 \cb3 \expnd0\expndtw0\kerning0
-printjson\uc0\u8203 (\u8203 db\u8203 .\u8203 people\u8203 .\u8203 find\u8203 (\{ birth_date\u8203 :\u8203 \{$gte\u8203 :\u8203 "\u8203 2000-01-01T00:00:00Z\u8203 "\u8203 \}\},\{first_name\u8203 :\u8203 1\u8203 , last_name\u8203 :\u8203 1\u8203 ,\u8203 "\u8203 location.city\u8203 "\u8203 :\u8203 1\u8203 , _id\u8203 :\u8203 0\u8203  \}).\u8203 toArray\u8203 ())\
-\
-\
-\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural\partightenfactor0
 
-\fs32 \cf5 \cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 db.people.find(\{ birth_date: \{$gte: \'842000-01-01T00:00:00Z\'94 \}\}, \{ first_name: 1, last_name: 1, \'84location.city\'94: 1, _id: 0\})}
+db.people.find({ birth_date: {$gte: „2000-01-01T00:00:00Z” }}, { first_name: 1, last_name: 1, „location.city”: 1, _id: 0})
--- Zestawy/Ćwiczenia 3/zapytanie_6.js
+++ Zestawy/Ćwiczenia 3/zapytanie_6.js
@@ -1,62 +1,52 @@
-{\rtf1\ansi\ansicpg1250\cocoartf2512
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red255\green255\blue255;\red196\green26\blue22;
-}
-{\*\expandedcolortbl;;\csgenericrgb\c0\c0\c0\c85000;\csgenericrgb\c100000\c100000\c100000;\csgenericrgb\c77000\c10200\c8600;
-}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab642
-\pard\tx642\pardeftab642\pardirnatural\partightenfactor0
+db.people.insertOne({
+    "sex" : "Male",
+    "first_name" : "Szymon",
+    "last_name" : "Szysz",
+    "job" : "iOS Developer",
+    "email" : "xxxx@xxxx.pl",
+    "location" : {
+        "city" : "Warszawa",
+        "address" : {
+            "streetname" : "XXXXXXXX",
+            "streetnumber" : "XXXX"
+        }
+    },
+    "description" : "non velit nec nisi vulputate nonummy maecenas tincidunt lacus at velit",
+    "height" : "176.89",
+    "weight" : "83.8",
+    "birth_date" : "1996-11-25T11:51:38Z",
+    "nationality" : "Poland",
+    "credit" : [
+        {
+            "type" : "instapayment",
+            "number" : "3581609550232321672",
+            "currency" : "CZK",
+            "balance" : "5658.66"
+        },
+        {
+            "type" : "visa-electron",
+            "number" : "637979654232338409",
+            "currency" : "PLN",
+            "balance" : "4280.15"
+        },
+        {
+            "type" : "diners-club-international",
+            "number" : "3539516232040777020",
+            "currency" : "LKR",
+            "balance" : "5402.31"
+        },
+        {
+            "type" : "jcb",
+            "number" : "56022234477278262112",
+            "currency" : "CNY",
+            "balance" : "4653.86"
+        },
+        {
+            "type" : "jcb",
+            "number" : "3529854509994014122",
+            "currency" : "ARS",
+            "balance" : "4944.8"
+        }
+    ]
+})
 
-\f0\fs26 \cf2 \cb3 db.people.insertOne(\{\
-    \cf4 "sex"\cf2  : \cf4 "Male"\cf2 ,\
-    \cf4 "first_name"\cf2  : \cf4 "Szymon"\cf2 ,\
-    \cf4 "last_name"\cf2  : \cf4 "Szysz"\cf2 ,\
-    \cf4 "job"\cf2  : \cf4 "iOS Developer"\cf2 ,\
-    \cf4 "email"\cf2  : \cf4 "xxxx@xxxx.pl"\cf2 ,\
-    \cf4 "location"\cf2  : \{\
-        \cf4 "city"\cf2  : \cf4 "Warszawa"\cf2 ,\
-        \cf4 "address"\cf2  : \{\
-            \cf4 "streetname"\cf2  : \cf4 "XXXXXXXX"\cf2 ,\
-            \cf4 "streetnumber"\cf2  : \cf4 "XXXX"\cf2 \
-        \}\
-    \},\
-    \cf4 "description"\cf2  : \cf4 "non velit nec nisi vulputate nonummy maecenas tincidunt lacus at velit"\cf2 ,\
-    \cf4 "height"\cf2  : \cf4 "176.89"\cf2 ,\
-    \cf4 "weight"\cf2  : \cf4 "83.8"\cf2 ,\
-    \cf4 "birth_date"\cf2  : \cf4 "1996-11-25T11:51:38Z"\cf2 ,\
-    \cf4 "nationality"\cf2  : \cf4 "Poland"\cf2 ,\
-    \cf4 "credit"\cf2  : [\
-        \{\
-            \cf4 "type"\cf2  : \cf4 "instapayment"\cf2 ,\
-            \cf4 "number"\cf2  : \cf4 "3581609550232321672"\cf2 ,\
-            \cf4 "currency"\cf2  : \cf4 "CZK"\cf2 ,\
-            \cf4 "balance"\cf2  : \cf4 "5658.66"\cf2 \
-        \},\
-        \{\
-            \cf4 "type"\cf2  : \cf4 "visa-electron"\cf2 ,\
-            \cf4 "number"\cf2  : \cf4 "637979654232338409"\cf2 ,\
-            \cf4 "currency"\cf2  : \cf4 "PLN"\cf2 ,\
-            \cf4 "balance"\cf2  : \cf4 "4280.15"\cf2 \
-        \},\
-        \{\
-            \cf4 "type"\cf2  : \cf4 "diners-club-international"\cf2 ,\
-            \cf4 "number"\cf2  : \cf4 "3539516232040777020"\cf2 ,\
-            \cf4 "currency"\cf2  : \cf4 "LKR"\cf2 ,\
-            \cf4 "balance"\cf2  : \cf4 "5402.31"\cf2 \
-        \},\
-        \{\
-            \cf4 "type"\cf2  : \cf4 "jcb"\cf2 ,\
-            \cf4 "number"\cf2  : \cf4 "56022234477278262112"\cf2 ,\
-            \cf4 "currency"\cf2  : \cf4 "CNY"\cf2 ,\
-            \cf4 "balance"\cf2  : \cf4 "4653.86"\cf2 \
-        \},\
-        \{\
-            \cf4 "type"\cf2  : \cf4 "jcb"\cf2 ,\
-            \cf4 "number"\cf2  : \cf4 "3529854509994014122"\cf2 ,\
-            \cf4 "currency"\cf2  : \cf4 "ARS"\cf2 ,\
-            \cf4 "balance"\cf2  : \cf4 "4944.8"\cf2 \
-        \}\
-    ]\
-\})\
-}
